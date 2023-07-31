823698f062b94b48ce5e119aff1b16120bdad3eb
Nitrick <shrivastava.aditya002@gmail.com>
2023-07-31 13:54:27 +0530

    changes of new points

--- DiskOperationManagementApp/Form1.Designer.cs
+++ DiskOperationManagementApp/Form1.Designer.cs
@@ -30,35 +30,81 @@
         {
             this.btnInstall = new System.Windows.Forms.Button();
             this.btnUninstall = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnInstall
             // 
-            this.btnInstall.Location = new System.Drawing.Point(130, 177);
+            this.btnInstall.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnInstall.FlatAppearance.BorderSize = 0;
+            this.btnInstall.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnInstall.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInstall.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnInstall.Location = new System.Drawing.Point(355, 315);
+            this.btnInstall.Margin = new System.Windows.Forms.Padding(2);
             this.btnInstall.Name = "btnInstall";
-            this.btnInstall.Size = new System.Drawing.Size(75, 23);
+            this.btnInstall.Size = new System.Drawing.Size(90, 40);
             this.btnInstall.TabIndex = 0;
             this.btnInstall.Text = "Install";
-            this.btnInstall.UseVisualStyleBackColor = true;
+            this.btnInstall.UseVisualStyleBackColor = false;
             this.btnInstall.Click += new System.EventHandler(this.btnInstall_Click);
             // 
             // btnUninstall
             // 
-            this.btnUninstall.Location = new System.Drawing.Point(339, 177);
+            this.btnUninstall.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUninstall.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUninstall.Location = new System.Drawing.Point(459, 315);
+            this.btnUninstall.Margin = new System.Windows.Forms.Padding(2);
             this.btnUninstall.Name = "btnUninstall";
-            this.btnUninstall.Size = new System.Drawing.Size(75, 23);
+            this.btnUninstall.Size = new System.Drawing.Size(130, 40);
             this.btnUninstall.TabIndex = 1;
             this.btnUninstall.Text = "UnInstall";
             this.btnUninstall.UseVisualStyleBackColor = true;
             this.btnUninstall.Click += new System.EventHandler(this.btnUninstall_Click);
             // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.ForestGreen;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(151, 315);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 40);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Encrypt";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Encrypt_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DarkOrange;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(255, 315);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 40);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Decrypt";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.Decypt_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnUninstall);
             this.Controls.Add(this.btnInstall);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -69,6 +115,8 @@
 
         private System.Windows.Forms.Button btnInstall;
         private System.Windows.Forms.Button btnUninstall;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- DiskOperationManagementApp/Form1.cs
+++ DiskOperationManagementApp/Form1.cs
@@ -9,11 +9,25 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Diagnostics;
 using System.ServiceProcess;
+using System.Security.Cryptography;
+using System.Reflection.Emit;
+using System.IO;
 
 namespace DiskOperationManagementApp
 {
     public partial class Form1 : Form
     {
+
+        readonly CspParameters _cspp = new CspParameters();
+        RSACryptoServiceProvider _rsa;
+
+        const string EncrFolder = @"F:\Encrypt\";
+        const string DecrFolder = @"F:\Encrypt\";
+        const string SrcFolder = @"c:\docs\";
+
+        const string PubKeyFile = @"F:\Encrypt\rsaPublicKey.txt";
+
+        const string KeyName = "Key01";
         public Form1()
         {
             InitializeComponent();
@@ -38,7 +52,7 @@ namespace DiskOperationManagementApp
             string serviceName = "DiskOperationManagement";
 
             // Replace "PathToYourWorkerServiceExecutable" with the full path to the Worker Service executable.
-            string servicePath = "E:\\IvotionTech\\Project\\DiskOperationManagement\\DiskOperationService\\bin\\Debug\\net6.0\\DiskOperationService.exe";
+            string servicePath = "F:\\Aditya\\Disk_Operations_Monitoring\\DiskOperationManagement\\DiskOperationService\\bin\\Debug\\net6.0\\DiskOperationService.exe";
 
             try
             {
@@ -66,6 +80,209 @@ namespace DiskOperationManagementApp
         private bool ServiceExists(string serviceName)
         {
             return ServiceController.GetServices().Any(service => service.ServiceName == serviceName);
+        }
+
+        private void CreateAsmKeys()
+        {
+            // Stores a key pair in the key container.
+            _cspp.KeyContainerName = KeyName;
+            _rsa = new RSACryptoServiceProvider(_cspp)
+            {
+                PersistKeyInCsp = true
+            };
+        }
+
+        private void Encrypt_Click(object sender, EventArgs e)
+        {
+            CreateAsmKeys();
+            var logfile = "F:\\Encrypt\\log.txt";
+            EncryptFile(logfile);
+        }
+
+
+        private void EncryptFile(string fileInfo)
+        {
+            // Create instance of Aes for
+            // symmetric encryption of the data.
+            var fileName = Path.GetFileName(fileInfo);
+            var fileFullName = Path.GetFullPath(fileInfo);
+            FileInfo file = new FileInfo(fileName);
+            Aes aes = Aes.Create();
+            ICryptoTransform transform = aes.CreateEncryptor();
+
+            // Use RSACryptoServiceProvider to
+            // encrypt the AES key.
+            // rsa is previously instantiated:
+            //    rsa = new RSACryptoServiceProvider(cspp);
+            byte[] keyEncrypted = _rsa.Encrypt(aes.Key, false);
+
+            // Create byte arrays to contain
+            // the length values of the key and IV.
+            int lKey = keyEncrypted.Length;
+            byte[] LenK = BitConverter.GetBytes(lKey);
+            int lIV = aes.IV.Length;
+            byte[] LenIV = BitConverter.GetBytes(lIV);
+
+            // Write the following to the FileStream
+            // for the encrypted file (outFs):
+            // - length of the key
+            // - length of the IV
+            // - encrypted key
+            // - the IV
+            // - the encrypted cipher content
+
+            // Change the file's extension to ".enc"
+            string outFile =
+                Path.Combine(EncrFolder, Path.ChangeExtension(file.Name, ".enc"));
+
+            using (var outFs = new FileStream(outFile, FileMode.Create))
+            {
+                outFs.Write(LenK, 0, 4);
+                outFs.Write(LenIV, 0, 4);
+                outFs.Write(keyEncrypted, 0, lKey);
+                outFs.Write(aes.IV, 0, lIV);
+
+                // Now write the cipher text using
+                // a CryptoStream for encrypting.
+                using (var outStreamEncrypted =
+                    new CryptoStream(outFs, transform, CryptoStreamMode.Write))
+                {
+                    // By encrypting a chunk at
+                    // a time, you can save memory
+                    // and accommodate large files.
+                    int count = 0;
+                    int offset = 0;
+
+                    // blockSizeBytes can be any arbitrary size.
+                    int blockSizeBytes = aes.BlockSize / 8;
+                    byte[] data = new byte[blockSizeBytes];
+                    int bytesRead = 0;
+
+                    using (var inFs = new FileStream(fileFullName, FileMode.Open))
+                    {
+                        do
+                        {
+                            count = inFs.Read(data, 0, blockSizeBytes);
+                            offset += count;
+                            outStreamEncrypted.Write(data, 0, count);
+                            bytesRead += blockSizeBytes;
+                        } while (count > 0);
+                    }
+                    outStreamEncrypted.FlushFinalBlock();
+                }
+            }
+            Directory.CreateDirectory(EncrFolder);
+            using (var sw = new StreamWriter(PubKeyFile, false))
+            {
+                sw.Write(_rsa.ToXmlString(false));
+            }
+        }
+
+        private void Decypt_Click(object sender, EventArgs e)
+        {
+            var logfile = "F:\\Encrypt\\log.enc";
+            DecryptFile(logfile);
+        }
+
+        private void DecryptFile(string fileInfo)
+        {
+            _cspp.KeyContainerName = KeyName;
+            _rsa = new RSACryptoServiceProvider(_cspp)
+            {
+                PersistKeyInCsp = true
+            };
+
+            // Create instance of Aes for
+            // symmetric decryption of the data.
+            Aes aes = Aes.Create();
+            var fileName = Path.GetFileName(fileInfo);
+            var fileFullName = Path.GetFullPath(fileInfo);
+            // Create byte arrays to get the length of
+            // the encrypted key and IV.
+            // These values were stored as 4 bytes each
+            // at the beginning of the encrypted package.
+            byte[] LenK = new byte[4];
+            byte[] LenIV = new byte[4];
+
+            // Construct the file name for the decrypted file.
+            string outFile =
+                Path.ChangeExtension(fileFullName.Replace("log","log1"), ".txt");
+
+            // Use FileStream objects to read the encrypted
+            // file (inFs) and save the decrypted file (outFs).
+            using (var inFs = new FileStream(fileFullName, FileMode.Open))
+            {
+                inFs.Seek(0, SeekOrigin.Begin);
+                inFs.Read(LenK, 0, 3);
+                inFs.Seek(4, SeekOrigin.Begin);
+                inFs.Read(LenIV, 0, 3);
+
+                // Convert the lengths to integer values.
+                int lenK = BitConverter.ToInt32(LenK, 0);
+                int lenIV = BitConverter.ToInt32(LenIV, 0);
+
+                // Determine the start position of
+                // the cipher text (startC)
+                // and its length(lenC).
+                int startC = lenK + lenIV + 8;
+                int lenC = (int)inFs.Length - startC;
+
+                // Create the byte arrays for
+                // the encrypted Aes key,
+                // the IV, and the cipher text.
+                byte[] KeyEncrypted = new byte[lenK];
+                byte[] IV = new byte[lenIV];
+
+                // Extract the key and IV
+                // starting from index 8
+                // after the length values.
+                inFs.Seek(8, SeekOrigin.Begin);
+                inFs.Read(KeyEncrypted, 0, lenK);
+                inFs.Seek(8 + lenK, SeekOrigin.Begin);
+                inFs.Read(IV, 0, lenIV);
+
+                Directory.CreateDirectory(DecrFolder);
+                // Use RSACryptoServiceProvider
+                // to decrypt the AES key.
+                byte[] KeyDecrypted = _rsa.Decrypt(KeyEncrypted, false);
+
+                // Decrypt the key.
+                ICryptoTransform transform = aes.CreateDecryptor(KeyDecrypted, IV);
+
+                // Decrypt the cipher text from
+                // from the FileSteam of the encrypted
+                // file (inFs) into the FileStream
+                // for the decrypted file (outFs).
+                using (var outFs = new FileStream(outFile, FileMode.Create))
+                {
+                    int count = 0;
+                    int offset = 0;
+
+                    // blockSizeBytes can be any arbitrary size.
+                    int blockSizeBytes = aes.BlockSize / 8;
+                    byte[] data = new byte[blockSizeBytes];
+
+                    // By decrypting a chunk a time,
+                    // you can save memory and
+                    // accommodate large files.
+
+                    // Start at the beginning
+                    // of the cipher text.
+                    inFs.Seek(startC, SeekOrigin.Begin);
+                    using (var outStreamDecrypted =
+                        new CryptoStream(outFs, transform, CryptoStreamMode.Write))
+                    {
+                        do
+                        {
+                            count = inFs.Read(data, 0, blockSizeBytes);
+                            offset += count;
+                            outStreamDecrypted.Write(data, 0, count);
+                        } while (count > 0);
+
+                        outStreamDecrypted.FlushFinalBlock();
+                    }
+                }
+            }
         }
     }
 }
